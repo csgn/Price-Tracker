cddf54bfb91e10beef3b058bbbe95f18868127b9
csgn <dev.csgn@gmail.com>
2021-11-07 01:13:31 +0300

    Reload something

--- Source/Driver/driver/database/insertions.py
+++ Source/Driver/driver/database/insertions.py
@@ -1,42 +1,86 @@
 from typing import Dict, Any
-from datetime import date
+from datetime import datetime
 
 import database as db
 
 
-def __INSERT__PRODUCT():
-    pass
+def gettimestamp():
+    return (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def __INSERT__BRAND():
-    pass
+def getid(fun) -> int:
+    def wrapper(*args, **kwargs):
+        fun(args if len(args) > 1 else args[0])
+        return db.cursor.fetchone()[0]
+
+    return wrapper
 
 
-def __INSERT__PRICE():
-    pass
+@getid
+def __INSERT__PRODUCT(*args) -> None:
+    rateid, brandid, priceid, categoryid, name, url, images = args[0]
+    images = '{' + ",".join(f'"{image}"' for image in images) + '}'
+
+    db.cursor.execute(f"""
+    INSERT INTO product (name, images, url, rateid, priceid, brandid, categoryid)
+        VALUES (
+            '{name}',
+            '{images}',
+            '{url}',
+            {rateid},
+            {priceid},
+            {brandid},
+            {categoryid}
+        )
+        RETURNING productid;
+    """)
 
 
-def __INSERT__RATE(rate: str) -> int:
-    from datetime import datetime
-    current_date = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+@getid
+def __INSERT__BRAND(brand) -> None:
+    name, url = brand.values()
+
+    db.cursor.execute(f"""
+    INSERT INTO brand (name, url)
+        VALUES (
+            '{name}',
+            '{url}'
+        )
+        RETURNING brandid;
+    """)
+
+
+@getid
+def __INSERT__PRICE(price: str) -> None:
+    amount = float(price.split(',')[0])
+    current_date = gettimestamp()
+
+    db.cursor.execute(f"""
+    INSERT INTO price (amount, startdate)
+        VALUES (
+            '{amount}',
+            to_timestamp('{current_date}', 'yyyy-mm-dd hh24:mi:ss')
+        )
+        RETURNING priceid;
+    """)
+
+
+@getid
+def __INSERT__RATE(rate: str) -> None:
+    current_date = gettimestamp()
 
     db.cursor.execute(f"""
     INSERT INTO rate (score, date)
         VALUES (
-            {float(rate) if rate else '0.0'},
+            {float(rate) if rate else 0},
             to_timestamp('{current_date}', 'yyyy-mm-dd hh24:mi:ss')
         )
         RETURNING rateid;
     """)
 
-    return db.cursor.fetchone()[0]
 
-
-def __INSERT_IMAGES():
-    pass
-
-
-def __INSERT__SUPPLIER(supplier) -> int:
+@getid
+def __INSERT__SUPPLIER(supplier) -> None:
     name, rate, url, _ = supplier.values()
 
     db.cursor.execute(f"""
@@ -49,21 +93,62 @@ def __INSERT__SUPPLIER(supplier) -> int:
         RETURNING supplierid;
     """)
 
-    return db.cursor.fetchone()[0]
+
+@getid
+def __INSERT__CATEGORY(*args) -> None:
+    category, subcategories_id = args[0]
+    name, url = category.values()
+
+    for subcategoryid in subcategories_id:
+        db.cursor.execute(f"""
+        INSERT INTO category (name, url, subcategoryid)
+            VALUES (
+                '{name}',
+                '{url}',
+                {subcategoryid}
+            )
+            RETURNING categoryid;
+        """)
 
 
-def __INSERT__CATEGORY():
-    pass
+@getid
+def __INSERT__SUBCATEGORY(subcategory) -> None:
+    name, url = subcategory.values()
 
-
-def __INSERT__SUBCATEGORY():
-    pass
+    db.cursor.execute(f"""
+    INSERT INTO subcategory (name, url)
+        VALUES (
+            '{name}',
+            '{url}'
+        )
+        RETURNING subcategoryid;
+    """)
 
 
 def insert_to_db(other):
     product_rateid = __INSERT__RATE(other["rate"])
+    product_brandid = __INSERT__BRAND(other["brand"])
 
+    subcategories_id = []
+    for subcategory in other["subcategory"]:
+        subcategoryid = __INSERT__SUBCATEGORY(subcategory)
+        subcategories_id.append(subcategoryid)
+
+    product_categoryid = __INSERT__CATEGORY(
+        other["category"], subcategories_id)
+
+    product_priceid = __INSERT__PRICE(other["price"])
+
+    suppliers_id = []
     for supplier in other["supplier"]:
         supplierid = __INSERT__SUPPLIER(supplier)
+        suppliers_id.append(supplierid)
+
+    productid = __INSERT__PRODUCT(
+        product_rateid, product_brandid,
+        product_priceid, product_categoryid,
+        other["product name"], other["url"],
+        other["images"]
+    )
 
     db.connection.commit()
