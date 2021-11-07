66ab53ffc03428aa024495deead145a41a3e6c8d
csgn <dev.csgn@gmail.com>
2021-11-08 02:52:32 +0300

    Reload logger

--- Source/Driver/driver/conf/logger.py
+++ Source/Driver/driver/conf/logger.py
@@ -5,14 +5,14 @@ from colorama import Fore, Back, Style
 
 def info(var, message):
     print(
-        f"{time.now()} - {Fore.LIGHTBLUE_EX + '[INFO]' + Style.RESET_ALL}{'':>2}:\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message}")
+        f"{time.now()} - {Fore.LIGHTBLUE_EX + '[INFO]' + Style.RESET_ALL}{'':>2}:\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message.capitalize()}")
 
 
 def warning(var, message):
     print(
-        f"{time.now()} - {Fore.LIGHTYELLOW_EX + '[WARNING]' + Style.RESET_ALL} :\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message}")
+        f"{time.now()} - {Fore.LIGHTYELLOW_EX + '[WARNING]' + Style.RESET_ALL} :\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message.capitalize()}")
 
 
 def error(var, message):
     print(
-        f"{time.now()} - {Fore.LIGHTRED_EX + '[ERROR]' + Style.RESET_ALL} :\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message}")
+        f"{time.now()} - {Fore.LIGHTRED_EX + '[ERROR]' + Style.RESET_ALL} :\t{Fore.LIGHTWHITE_EX + var + Style.RESET_ALL + ' ' + message.capitalize()}")
